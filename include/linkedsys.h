d3461a834507b04e682946456bde07b69d878aea
crispeeweevile <crispeeweevile@gmail.com>
2023-07-25 16:42:40 -0500

    fError struct various errors

--- include/linkedsys.h
+++ include/linkedsys.h
@@ -1,3 +1,13 @@
+#include <stdbool.h>
+#define METHOD_FAIL NULL;
+
+typedef enum fError {
+    FSUCCESS=0, // general success
+    FFAILURE=-1, // encompasses all errors
+    FBADARG=-2, // encompasses all bad arg errors
+    FNULLARG=-3, // only on null arg errors
+} fError;
+
 typedef struct Data {
     int money;
     int items;
@@ -9,3 +19,6 @@ typedef struct Node {
     Node *prev;
     Node *next;
 } Node;
+
+Data *new_data(int iMoney, int iItems, int iDebt);
+
