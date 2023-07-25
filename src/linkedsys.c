c2b9236c4257e27ac87e899bc34ef0f907ea7919
crispeeweevile <crispeeweevile@gmail.com>
2023-07-25 17:21:30 -0500

    began implementing functions

--- src/linkedsys.c
+++ src/linkedsys.c
@@ -1,3 +1,53 @@
 #include "linkedsys.h"
+#include "general_utils.h"
+
+Data *new_data(int iMoney, int iItems, int iDebt) {
+    Data *nData = smalloc(sizeof(Data));
+    nData->money = iMoney;
+    nData->items = iItems;
+    nData->debt = iDebt;
+
+    return nData;
+}
+
+fError free_data(Data **data) {
+    
+}
 
 
+LList *new_llist(Data *data) {
+
+}
+
+fError free_llist(LList **list) {
+
+}
+
+Node *insert_at_tail(LList **list, Node *node) {
+
+}
+
+
+Node *new_node(Data *data) {
+
+}
+
+fError free_node(Node **node) {
+
+}
+
+Node *goto_next(LList **list) {
+
+}
+
+Node *goto_prev(LList **list) {
+
+}
+
+Node *goto_first(LList **list) {
+
+}
+
+Node *goto_last(LList **list) {
+
+}
