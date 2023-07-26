d51466a196bcd4001511c89959d53873e596c991
crispeeweevile <crispeeweevile@gmail.com>
2023-07-26 00:41:08 -0500

    cleaned up some code renamed the official struct name (not the typedef)

--- include/linkedsys.h
+++ include/linkedsys.h
@@ -1,26 +1,26 @@
 #include <stdbool.h>
 #define METHOD_FAIL NULL;
 
-typedef enum fError {
-    FSUCCESS=0, // general success
-    FFAILURE=-1, // encompasses all errors
-    FBADARG=-2, // encompasses all bad arg errors
-    FNULLARG=-3, // only on null arg errors
+typedef enum efError {
+    FSUCCESS = 0, // general success
+    FFAILURE = -1, // encompasses all errors
+    FBADARG  = -2, // encompasses all bad arg errors
+    FNULLARG = -3, // only on null arg errors
 } fError;
 
-typedef struct Data {
+typedef struct sData {
     int money;
     int items;
     int debt;
 } Data;
 
-typedef struct Node {
+typedef struct sNode {
     Data *data;
-    Node *prev;
-    Node *next;
+    struct sNode *prev;
+    struct sNode *next;
 } Node;
 
-typedef struct LList {
+typedef struct sLList {
     Node *current;
     Node *head;
     Node *tail;
